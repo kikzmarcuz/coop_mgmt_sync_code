7e819b6cd5a1907470a8dbeb50a239ca81a7542b
kikzmarcuz <46095504+kikzmarcuz@users.noreply.github.com>
2019-02-02 20:28:33 +0800

    DELETE FUNCTION

--- samplemwmmpc/creditOperationReport.php
+++ samplemwmmpc/creditOperationReport.php
@@ -1721,8 +1721,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     <tr>
                         <th></th>
                         <th></th>
-                        <th></th>
-                        <th></th>
                         <th>ID Number | Chart_of_Account_Code</th>
                         <th>Name | Chart_of_Account_Title_______________</th>
                         <th>Description | Remarks</th>
@@ -1751,9 +1749,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                         echo "<tr>";
                             echo "<td>  <button class =". "deletebutton". " "  . "type =" . "submit" . " " . " " ."value=". "$transactionnumber[$counterh]_$tn[$counterh]_$loanApplicationNumber[$counterh]" . " " . "name=" . "clearOR". ">"  . "CLEAR" . " </button> </td>";
                             echo "<td> <button class =". "deletebutton". " " . "type =" . "submit" . " " . " " ."value=". "$tn[$counterh]" . " " . "name=" . "cancelOR" . ">"  . "CANCEL" . " </button> </td>";
-
-                             echo "<td>" . $tn[$counterh] . "</td>";
-                            echo "<td>" . $transactionnumber[$counterh] . "</td>";
                             echo "<td>" . $idNumber[$counterh] . "</td>";
                             echo "<td>" . $fullNameD[$counterh] . "</td>";
                             echo "<td>" . $loanApplicationNumber[$counterh] . "</td>";
